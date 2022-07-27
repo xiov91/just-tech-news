8de41b74c77b84faabcfb710ceb73f9aa107c48c
xiov91 <99517717+xiov91@users.noreply.github.com>
2022-07-26 21:52:01 -0400

    lesson 2 finish

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-
-// GET /api/users
+// get all users
 router.get('/', (req, res) => {
-  // Access our User model and run .findAll() method)
   User.findAll({
     attributes: { exclude: ['password'] }
   })
@@ -15,7 +13,6 @@ router.get('/', (req, res) => {
     });
 });
 
-// GET /api/users/1
 router.get('/:id', (req, res) => {
   User.findOne({
     attributes: { exclude: ['password'] },
@@ -36,9 +33,8 @@ router.get('/:id', (req, res) => {
     });
 });
 
-// POST /api/users
 router.post('/', (req, res) => {
-  // expects {username: '', email: '@email.com', password: 'password`'}
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
     username: req.body.username,
     email: req.body.email,
@@ -51,8 +47,32 @@ router.post('/', (req, res) => {
     });
 });
 
-// PUT /api/users/1
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
+});
+
 router.put('/:id', (req, res) => {
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
     individualHooks: true,
@@ -60,7 +80,6 @@ router.put('/:id', (req, res) => {
       id: req.params.id
     }
   })
-
     .then(dbUserData => {
       if (!dbUserData[0]) {
         res.status(404).json({ message: 'No user found with this id' });
@@ -74,7 +93,6 @@ router.put('/:id', (req, res) => {
     });
 });
 
-// DELETE /api/users/1
 router.delete('/:id', (req, res) => {
   User.destroy({
     where: {
